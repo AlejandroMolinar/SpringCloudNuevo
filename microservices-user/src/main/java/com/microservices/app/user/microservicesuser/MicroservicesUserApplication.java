617b3265097d35d4b70af55920f7738ad8c8c9fe
Alejandro <alejandro.molinar17@gmail.com>
2022-06-08 21:30:12 -0400

    MicroServices-User

---
+++ microservices-user/src/main/java/com/microservices/app/user/microservicesuser/MicroservicesUserApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.app.user.microservicesuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesUserApplication.class, args);
+	}
+
+}
---
+++ microservices-user/src/test/java/com/microservices/app/user/microservicesuser/MicroservicesUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.app.user.microservicesuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
