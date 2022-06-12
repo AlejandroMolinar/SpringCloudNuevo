e95da12985a3644e77a6e2e4cd181851a7535044
Alejandro <alejandro.molinar17@gmail.com>
2022-06-12 17:54:47 -0400

    Eureka Server

---
+++ microservices-eureka/src/test/java/com/microservices/app/eureka/microserviceseureka/MicroservicesEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.app.eureka.microserviceseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
