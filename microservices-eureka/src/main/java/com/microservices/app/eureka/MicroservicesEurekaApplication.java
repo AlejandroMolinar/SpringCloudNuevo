7393fee2e0a920792dde551832f6b3d39c8aa69b
Alejandro <alejandro.molinar17@gmail.com>
2022-06-12 19:15:18 -0400

    Eureka Ok

--- microservices-eureka/src/main/java/com/microservices/app/eureka/MicroservicesEurekaApplication.java
+++ microservices-eureka/src/main/java/com/microservices/app/eureka/MicroservicesEurekaApplication.java
@@ -2,7 +2,9 @@ package com.microservices.app.microserviceseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MicroservicesEurekaApplication {
 
