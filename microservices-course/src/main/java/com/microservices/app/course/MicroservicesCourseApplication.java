09a0c75c7fddad43a2ada468364b17806b01ab9c
Alejandro <alejandro.molinar17@gmail.com>
2022-07-03 14:58:15 -0400

    microservices-course

---
+++ microservices-course/src/main/java/com/microservices/app/course/MicroservicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.app.course.microservicescourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCourseApplication.class, args);
+	}
+
+}
---
+++ microservices-course/src/test/java/com/microservices/app/course/MicroservicesCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.app.course.microservicescourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
