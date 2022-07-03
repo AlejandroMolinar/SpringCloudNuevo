5461615677a22f9e7fa56ce95444aa8725cb929c
Alejandro <alejandro.molinar17@gmail.com>
2022-07-03 15:44:20 -0400

    Errores de Version

--- microservices-course/src/main/java/com/microservices/app/course/models/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.app.user.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.microservices.app.user.models.entity.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long>{
-    
-}
--- microservices-course/src/main/java/com/microservices/app/course/service/CourseService.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.app.user.service;
-
-import com.microservices.app.commons.service.CommonService;
-import com.microservices.app.user.models.entity.Student;
-
-public interface StudentService extends CommonService<Student>{
-        
-}
