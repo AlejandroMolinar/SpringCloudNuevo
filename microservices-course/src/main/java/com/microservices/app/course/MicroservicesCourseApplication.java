e2c54d2495ca7e77535c80f095ebccc88b40e46b
AlejandroMolinar <alejandro.molinar17@gmail.com>
2022-09-29 21:02:59 -0400

    Course

--- microservices-course/src/main/java/com/microservices/app/course/MicroservicesCourseApplication.java
+++ microservices-course/src/main/java/com/microservices/app/course/MicroservicesCourseApplication.java
@@ -1,4 +1,4 @@
-package com.microservices.app.course.microservicescourse;
+package com.microservices.app.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- microservices-course/src/test/java/com/microservices/app/course/MicroservicesCourseApplicationTests.java
+++ microservices-course/src/test/java/com/microservices/app/course/MicroservicesCourseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microservices.app.course.microservicescourse;
+package com.microservices.app.course;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
